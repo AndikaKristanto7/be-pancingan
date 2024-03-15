7df6a7f7168d357c6a02af1597921acc8549f1ce
Daimonda <monda2607@gmail.com>
2024-03-15 23:30:28 +0700

    test commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const router = require('./router.js')
 app.use(function (req, res, next) {
 
+    
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
 
