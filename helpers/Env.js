b9edd3919aed899617f117e7aa04ab1490a02af6
AndikaKristanto7 <andikakristanto789@gmail.com>
2024-05-03 18:49:55 +0700

    Add Env.js

---
+++ helpers/Env.js
@@ -0,0 +1,19 @@
+const axios =  require('axios')
+require('dotenv').config()
+
+module.exports = function(){
+    this.getEnv = function(param){
+      if(process.env.NODE_ENV != "development"){
+        isProd = true
+        isDev = false
+        axios.get("https://asia-southeast2-vps-binar.cloudfunctions.net/env-be-app",{
+            params:{
+                key: param
+            }
+        }).then((resp)=>{
+            return resp ?? false 
+        })
+      }
+      return process.env[param]
+    }
+}
